59801b94e87a2ba7f662db9c32916bb9158fd887
Mateumiralles <mateumiralles714@gmail.com>
2022-05-03 12:45:21 +0200

    -GestureDetector onTap Maoñ a recievedMailsList

--- lib/widgets/received_mails_list.dart
+++ lib/widgets/received_mails_list.dart
@@ -56,52 +56,76 @@ class _ReceivedMailsListState extends State<ReceivedMailsList> {
             child: ListView.separated(
               itemBuilder: (context, index) {
                 return Container(
-                  color: index%2==0 ? Colors.blue.shade50 : Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 20),
-                          child: receivedMails[index].unread
-                              ? const Icon(Icons.mark_email_unread_rounded, color: Colors.blue)
-                              : const Icon(
-                                  Icons.email_rounded,
-                                  color: Colors.grey,
-                                ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  receivedMails[index].time.minute >= 10
-                                      ? Text(
-                                          '${receivedMails[index].time.day}/${receivedMails[index].time.month}/${receivedMails[index].time.year} - ${receivedMails[index].time.hour}:${receivedMails[index].time.minute}',
-                                          style: TextStyle(color: Colors.grey.shade600, fontSize: 12),
-                                        )
-                                      : Text(
-                                          '${receivedMails[index].time.day}/${receivedMails[index].time.month}/${receivedMails[index].time.year} - ${receivedMails[index].time.hour}:0${receivedMails[index].time.minute}',
-                                          style: TextStyle(color: Colors.grey.shade600, fontSize: 12),
-                                        ),
-                                  Text(
-                                    receivedMails[index].author,
-                                    style: TextStyle(color: Colors.grey.shade600, fontSize: 12),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 3),
-                              Text(receivedMails[index].subject,
-                                  style: const TextStyle(
-                                      fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),
-                                  textAlign: TextAlign.left),
-                            ],
+                  color: receivedMails[index].unread ? Colors.blue.shade50 : Colors.white,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return Scaffold(
+                          appBar: AppBar(
+                            title: const Text('MISSATGE DETAIL!'),
                           ),
-                        )
-                      ],
+                          body:  Center(
+                              child: Text('INFORMACIO DEL MISSATGE ${receivedMails[index].id}!')),
+                        );
+                      }));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: receivedMails[index].unread
+                                ? const Icon(Icons.mark_email_unread_rounded,
+                                    color: Colors.blue)
+                                : const Icon(
+                                    Icons.email_rounded,
+                                    color: Colors.grey,
+                                  ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    receivedMails[index].time.minute >= 10
+                                        ? Text(
+                                            '${receivedMails[index].time.day}/${receivedMails[index].time.month}/${receivedMails[index].time.year} - ${receivedMails[index].time.hour}:${receivedMails[index].time.minute}',
+                                            style: TextStyle(
+                                                color: Colors.grey.shade600,
+                                                fontSize: 12),
+                                          )
+                                        : Text(
+                                            '${receivedMails[index].time.day}/${receivedMails[index].time.month}/${receivedMails[index].time.year} - ${receivedMails[index].time.hour}:0${receivedMails[index].time.minute}',
+                                            style: TextStyle(
+                                                color: Colors.grey.shade600,
+                                                fontSize: 12),
+                                          ),
+                                    Text(
+                                      receivedMails[index].author,
+                                      style: TextStyle(
+                                          color: Colors.grey.shade600,
+                                          fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 3),
+                                Text(receivedMails[index].subject,
+                                    style: const TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.left),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 );
